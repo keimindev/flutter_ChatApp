358993a60c4f5a0ff442d11641758c1f45d02b3c
keira <keimindev@gmail.com>
2022-05-13 23:01:10 +0900

    01. Chat app - Login page UI

---
+++ lib/config/palette.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/painting.dart';
+
+class Palette{
+  static const Color iconColor = Color(0xffb6c7d1);
+  static const Color activeColor = Color(0xff09126c);
+  static const Color textColor1 = Color(0xffa7bcc7);
+  static const Color textColor2 = Color(0xff9bb3c0);
+  static const Color facebookColor = Color(0xff3b5999);
+  static const Color googleColor = Color(0xffde4b39);
+  static const Color backgroundColor = Color(0xffecf3f9);
+}
